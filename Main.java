73bae10436b9ad9e71e83e445af84514d23db639
DumbestPerson224 <eTmullall@outlook.com>
2025-01-20 11:49:13 -0500

    Updated stuff  (buggy no time to fix)

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 public class Main{
     private static Window window = null;;
     public static void main(String[] args) {
-        window = new Window("Hyper Visor", 500, 500);
-        window.setVisible(true);
-        window.setSize(500, 500);
-        window.setResizable(true);
+        
     }
 };
--- VirtualMachineUI.java
+++ VirtualMachineUI.java
@@ -1,30 +1,39 @@
 import javax.swing.JInternalFrame;
 import javax.swing.JComponent;
 import javax.swing.JMenuBar;
-public class internalWindows{
+import javax.swing.JLabel;
+public class VirtualMachineUI{
     public JInternalFrame internalFrame = null;
+    public JLabel label = null;
     public internalWindows(){
         internalFrame = new JInternalFrame();
+        label = new JLabel();
         internalFrame.setDefaultCloseOperation(JInternalFrame.DISPOSE_ON_CLOSE);
     }
 
-    void setSize(int width, int height){
+    public void setSize(int width, int height){
         internalFrame.setSize(width, height);
     }
 
-    void setResizable(boolean resizable){
+    public void setResizable(boolean resizable){
         internalFrame.setResizable(resizable);
     }
 
-    void show(boolean show){
+    public void show(boolean show){
         internalFrame.setVisible(show);
     }
-    void add(JComponent component){
+    
+    
+    public void add(JComponent component){
         internalFrame.add(component);
     }
 
-    void remove(JComponent component){
+    
+    public void remove(JComponent component){
         internalFrame.remove(component);
     }
 
+    public void setText(String text){
+        label.setText(text);
+    }
 }
