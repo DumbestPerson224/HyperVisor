7505cc74b647353d2f7c7ad4f2ac0dfe74d0c4be
DumbestPerson224 <eTmullall@outlook.com>
2025-02-01 00:06:42 -0500

    Update task.hpp

--- core/drivers/task.hpp
+++ core/drivers/task.hpp
@@ -2,31 +2,26 @@ struct VMManager {
 	int proc_id = 0;
 	int* proc_address = nullptr;
 };
-struct VMManager* vmManager = new VMManager;
 
-
+struct VMManager vmManager;
 /*
   @parameters (Reference) proc_number
   @parameters (Reference) proc_address
   @types int
   @returns vmManager
 */
-inline VMManager* set_process(int& proc_number,  int& proc_address){
-	if (vmManager != nullptr) {
-		vmManager->proc_id = proc_number;
-		vmManager->proc_address = &proc_address;
-		return vmManager;
-	}
+VMManager* set_process(int& proc_number, int& proc_address) {
+	vmManager.proc_id = proc_number;
+	vmManager.proc_address = &proc_address;
+	return &vmManager;
 }
 
-inline VMManager* clear_process() {
-	if (vmManager != nullptr) {
-		if (vmManager->proc_address != nullptr) {
-			vmManager->proc_id ^= vmManager->proc_id;
-			vmManager->proc_address = nullptr;
-		}
-		return vmManager;
+VMManager* clear_process() {
+	if (vmManager.proc_address != nullptr) {
+		vmManager.proc_address = nullptr;
 	}
+	std::cout << "Process cleared\n";
+	return &vmManager;
 }
 
 /*
@@ -34,18 +29,9 @@ inline VMManager* clear_process() {
    @types int
    @returns vmManager
 */
-inline VMManager* write_to_process(int& object)  {
-	if (vmManager != nullptr) {
-		if (vmManager->proc_address != nullptr) {
-			*vmManager->proc_address = object;
-		}
-		return vmManager;
+VMManager* write_to_process(int& object) {
+	if (vmManager.proc_address != nullptr) {
+		*vmManager.proc_address = object;
 	}
-}
-
-inline void Deallocate() {
-	if (vmManager != nullptr) {
-		vmManager->proc_address = nullptr;
-		delete vmManager;
-	}
+	return &vmManager;
 }
